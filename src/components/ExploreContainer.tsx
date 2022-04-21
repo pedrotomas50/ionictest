3133adb05eab54b22997bb5e4b7ffef3d61d0707
pedrotomas50 <pedrotomas50@hotmail.com>
2022-04-21 10:23:39 +0100

    first test

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -6,7 +6,7 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
       <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>Start with<a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
   );
 };
